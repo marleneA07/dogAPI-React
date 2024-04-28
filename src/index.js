5288e1632aa93c56d1e745a5b8f62c2540ad2260
Marlene Azevedo <44255118+marleneA07@users.noreply.github.com>
2024-04-27 23:08:41 -0700

    Update index.js
    cleaned up some code

--- src/index.js
+++ src/index.js
@@ -15,15 +15,3 @@ root.render(
 reportWebVitals();
 
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-// import './index.css'; //Modify
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
